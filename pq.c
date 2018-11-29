aa231c53999227de97edfc9cb08d1fa24e34f701
PoSheng Hsu <hsupos@flip2.engr.oregonstate.edu>
2018-11-28 20:09:35 -0800

    finished

--- pq.c
+++ pq.c
@@ -4,27 +4,97 @@
  * you need in this file to implement a priority queue.  Make sure to add your
  * name and @oregonstate.edu email address below:
  *
- * Name:
- * Email:
+ * Name:Po-Sheng Hsu
+ * Email:hsupos@oregonstate.edu
  */
 
 #include <stdlib.h>
+#include <assert.h>
 
 #include "pq.h"
+#include "dynarray.h"
 
 /*
  * This is the structure that represents a priority queue.  You must define
  * this struct to contain the data needed to implement a priority queue.
  */
-struct pq;
+struct pq{
+  struct dynarray* dy; 
+};
 
+struct pnode{ 
+  int priority;
+  void *value;
+};
+
+int pq_priority_comparing(struct pq* pq, int p1, int p2){
+  int temp;
+  struct pnode* n1 = NULL;
+  struct pnode* n2 = NULL;
+  if(dynarray_size(pq->dy)){
+    n1 = dynarray_get(pq->dy, p1);
+    n2 = dynarray_get(pq->dy, p2);
+    temp = n1->priority - n2->priority;
+    return temp;
+  }
+  else return -1; 
+};
+
+void pq_swap(struct pq* pq, int n1, int n2){ 
+  struct priority_value_node* temp1 = NULL;
+	struct priority_value_node* temp2 = NULL;
+	temp1 = dynarray_get(pq->dy, n1); 
+	temp2 = dynarray_get(pq->dy, n2); 
+	dynarray_set(pq->dy, n1, temp2); 
+	dynarray_set(pq->dy, n2, temp1);
+};
+
+void pq_adjust_front(struct pq* pq){ 
+	assert(pq);
+	int i=0, j=0, root=0, front=0, next=0;
+	int size;
+	size = dynarray_size(pq->dy);
+	while(i<(size-1) / 2){ 
+    root = i;
+		front = 2 * i + 1;
+		next = front + 1;
+		j = front;
+		if(pq_priority_comparing(pq,front,next) > 0){
+			j++;
+		}
+		if(pq_priority_comparing(pq, root, j) > 0){
+			pq_swap(pq, i, j);
+			i = j;
+		}
+		else break;
+	}
+}
+
+void pq_adjust_back(struct pq* pq){ 
+	assert(pq);
+	int i, root, leaf;
+	i = dynarray_size(pq->dy) - 2;
+	while(i > 0){
+		leaf = i;
+		root = (leaf - 1) / 2;
+		if(pq_priority_comparing(pq,root,leaf) > 0){
+			pq_swap(pq,leaf,root);
+			i = root;
+		}
+		else  
+		break;
+	}
+}
 
 /*
  * This function should allocate and initialize an empty priority queue and
  * return a pointer to it.
  */
 struct pq* pq_create() {
-  return NULL;
+  struct pq* pq = malloc(sizeof(struct pq));
+	assert(pq);
+	pq->dy = dynarray_create();
+	return pq;
 }
 
 
@@ -37,7 +107,15 @@ struct pq* pq_create() {
  *   pq - the priority queue to be destroyed.  May not be NULL.
  */
 void pq_free(struct pq* pq) {
-
+  assert(pq);
+  int i;
+	struct priority_value_node* node;
+	for(i=0;i<dynarray_size(pq->dy);i++){ 
+		node = dynarray_get(pq->dy,i);
+		free(node);
+	}
+	dynarray_free(pq->dy);
+	free(pq);
 }
 
 
@@ -53,7 +131,11 @@ void pq_free(struct pq* pq) {
  *   Should return 1 if pq is empty and 0 otherwise.
  */
 int pq_isempty(struct pq* pq) {
-  return 1;
+  assert(pq);
+	if( dynarray_size(pq->dy) == 0 )	
+	return 1; 
+	else  
+	return 0;
 }
 
 
@@ -75,7 +157,14 @@ int pq_isempty(struct pq* pq) {
  *     be the FIRST one returned.
  */
 void pq_insert(struct pq* pq, void* value, int priority) {
-
+  assert(pq);
+	int size;
+	struct pnode* node = malloc(sizeof(struct pnode));
+	assert(node);
+	node->priority = priority;
+	node->value = value;
+	dynarray_insert(pq->dy, -1, node); 
+	pq_adjust_back(pq);
 }
 
 
@@ -92,7 +181,13 @@ void pq_insert(struct pq* pq, void* value, int priority) {
  *   LOWEST priority value.
  */
 void* pq_first(struct pq* pq) {
-  return NULL;
+  assert(pq);
+	struct pnode* node = NULL;
+	if(dynarray_size(pq->dy)){
+		node = dynarray_get(pq->dy, 0);
+		return node->value;
+	}
+	return NULL;
 }
 
 
@@ -108,8 +203,14 @@ void* pq_first(struct pq* pq) {
  *   Should return the priority value of the first item in pq, i.e. the item
  *   with LOWEST priority value.
  */
-int pq_first_priority(struct pq* pq) {
-  return 0;
+int pq_first_priority(struct pq* pq){
+  assert(pq);
+	struct pnode* node = NULL;
+	if(dynarray_size(pq->dy)){
+		node = dynarray_get(pq->dy, 0);
+		return node->priority;
+	}
+	return -1;
 }
 
 
@@ -127,5 +228,20 @@ int pq_first_priority(struct pq* pq) {
  *   LOWEST priority value.
  */
 void* pq_remove_first(struct pq* pq) {
-  return NULL;
+  assert(pq);
+	struct pnode* node = NULL;
+	int* value;
+	int size;
+	size = dynarray_size(pq->dy);
+	if(size){
+		node = dynarray_get(pq->dy, 0);
+		pq_swap(pq,0,size - 1);
+		dynarray_remove(pq->dy,-1);
+		value = node->value;
+		free(node);
+		pq_adjust_front(pq);
+		return value;
+	}
+	
+	return NULL;
 }
